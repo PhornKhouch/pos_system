12ed442786a049204c25fd1c0a95f0a49130ca49
PhornKhouch <97729971+PhornKhouch@users.noreply.github.com>
2026-03-10 21:31:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const categoryRoute = require('./src/route/categoryRoute');
 const brandRoute = require('./src/route/brandRoute');
 const productRoute = require('./src/route/productRoute');
 const userRoutes = require('./src/route/userRoute');
-//const posSaleRoute = require('./src/route/posSaleRoute');
+const posSaleRoute = require('./src/route/posSaleRoute');
 const paymentMethodRoute = require('./src/route/paymentMethodRoute');
 const telegramConfigRoute = require('./src/route/telegramConfigRoute');
 const setAlertRoute = require('./src/route/setAlertRoute');
@@ -29,7 +29,7 @@ userRoutes(app);
 categoryRoute(app);
 brandRoute(app);
 productRoute(app);
-//posSaleRoute(app);
+posSaleRoute(app);
 paymentMethodRoute(app);
 telegramConfigRoute(app);
 setAlertRoute(app);
@@ -57,6 +57,7 @@ const startServer = async () => {
 
 // Start the application
 startServer();
+
 
 
 
